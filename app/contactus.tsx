4eadeb744f01220623f573ba1a742855e4e45e09
hassan <hassan.ali.khan161087@gmail.com>
2024-10-31 09:18:34 +0500

    first

---
+++ app/contactus.tsx
@@ -0,0 +1,8 @@
+export default function Contact () {
+    return (
+      <div>
+        <h1 className = "text-red-500"> This is an contact us page </h1>
+        <p> in this page we tell you about to contact the website onwer </p>
+      </div>
+    )
+  }
