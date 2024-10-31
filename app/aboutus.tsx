ef7a33ad29a9ab99afcdff9aff4e1b23e204660a
hassan <hassan.ali.khan161087@gmail.com>
2024-10-31 08:59:30 +0500

    firts

---
+++ app/aboutus.tsx
@@ -0,0 +1,8 @@
+export default function About () {
+    return (
+      <div>
+        <h1> This is an about us page </h1>
+        <p> in this page we tell you about the webside</p>
+      </div>
+    )
+  }
